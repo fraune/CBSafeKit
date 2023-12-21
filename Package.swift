04ddba4c57b1376b72a9be0fd8e744eef349777e
Brandon Fraune <brandon.fraune@gmail.com>
2023-12-20 22:01:10 -0600

    lint

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(
             name: "CBSafeKit",
             targets: ["CBSafeKit"]
-        ),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -28,6 +28,6 @@ let package = Package(
         .testTarget(
             name: "CBSafeKitTests",
             dependencies: ["CBSafeKit"]
-        ),
+        )
     ]
 )
